896c3d8d42aa0ec5ab920c49cc08dd7594bd30e2
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-05-07 02:52:30 -1100

    Add template movie data in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(title: 'Final Capstone', director: 'Team',
+  description: 'Now you know what it feels like to write rails',
+playing_time: '2pm', photo: 'no photo link', release_date: '6-4-20212', genre: 'classic',
+ticket_price: '$10', country: 'Nigeria', lead_cast: 'Busiwa Amadu Kingsley')
+
+Movie.create(title: 'Rails Final Capstone', director: 'Team',
+  description: 'Now you know what it feels like to write rails',
+playing_time: '2pm', photo: 'no photo link', release_date: '1-4-20212', genre: 'classic',
+ticket_price: '$10', country: 'South-Africa', lead_cast: 'Busiwa Amadu Kingsley')
