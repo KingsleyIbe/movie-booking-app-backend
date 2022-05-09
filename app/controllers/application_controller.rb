5707cc6e39b9264715fbc6ce7b702a2943b5ad08
Busiwa24 <busiwaliuma@gmail.com>
2022-05-09 15:11:27 +0200

    Add sign up details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,11 @@ include ActionController::MimeResponds
       
         before_action :authenticate_user!
         before_action :configure_permitted_parameters, if: :devise_controller?
+        
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+  end
+end
