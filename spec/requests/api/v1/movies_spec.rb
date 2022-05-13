e547cc0cce59d908af43291d44443b9b7507e2e0
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-05-13 09:35:53 +0100

    Add Integration files

---
+++ spec/requests/api/v1/movies_spec.rb
@@ -0,0 +1,38 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/movies', type: :request do
+
+  path '/api/v1/movies' do
+
+    get('list movies') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/movies/create' do
+
+    post('create movie') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
